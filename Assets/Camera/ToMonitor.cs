e11c18774d598403665c6765488d858d8966933a
julo5762 <oleszczakjulian@gmail.com>
2024-10-26 16:09:54 +0200

    change system of scene changing

--- Assets/Camera/ToMonitor.cs
+++ Assets/Camera/ToMonitor.cs
@@ -14,7 +14,7 @@ public class ToMonitor : MonoBehaviour
 
     public Button monitorClick;
 
-    
+    private RectTransform rectTransform;
     [SerializeField] private Camera cam;
 
 
@@ -22,6 +22,11 @@ public class ToMonitor : MonoBehaviour
     {
         monitorClick.onClick.AddListener(MonitorOpen);
         zoom = cam.orthographicSize;
+        rectTransform = GetComponent<RectTransform>();
+        float width = rectTransform.rect.width;
+        float height = rectTransform.rect.height;
+        
+        
     }
 
 
@@ -31,12 +36,12 @@ public class ToMonitor : MonoBehaviour
 
         if (!ToRoom.isZoomed && monitorClicked)
         {
+            Resize(2010f, 1130f, 0f);
             zoom = 2f;
             zoom = Mathf.Clamp(zoom, 2, 5);
             StartCoroutine(SceneDelay());
         } else if (ToRoom.isZoomed)
         {
-            Debug.Log("zoomed");
             zoom = 5f;
             ToRoom.isZoomed = false;
         }
@@ -47,11 +52,22 @@ public class ToMonitor : MonoBehaviour
     private IEnumerator SceneDelay()
     {
         yield return new WaitForSeconds(0.7f);
-        //SceneManager.LoadScene("Computer");
+        SceneManager.LoadScene("Computer");
     }
 
     public void MonitorOpen()
     {
         monitorClicked = true;
+    }
+
+    private void Resize(float newWidth, float newHeight, float yOffset)
+    {
+        // Zmiana rozmiaru
+        rectTransform.sizeDelta = new Vector2(newWidth, newHeight);
+        Debug.Log("Nowy rozmiar: szerokoœæ = " + newWidth + ", wysokoœæ = " + newHeight);
+
+        // Przesuniêcie obiektu w osi Y
+        rectTransform.anchoredPosition = new Vector2(0, yOffset);
+        Debug.Log("Nowa pozycja: " + rectTransform.anchoredPosition);
     }
 }
